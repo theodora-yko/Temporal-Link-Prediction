6a422fc726bc49c60622d1ee8b8982ddf5b45338
Theodora Ko <yko@wigglytuff.cs.uchicago.edu>
2024-04-13 19:51:51 -0500

    script for venv 3.8 for CSIL by Drew

---
+++ mac_setup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# setup dir
+mkdir ~/src && cd ~/src
+mkdir ~/.localpython
+
+# install python
+wget https://www.python.org/ftp/python/3.8.13/Python-3.8.13.tgz
+tar -zxvf Python-3.8.13.tgz
+cd Python-3.8.13
+./configure --prefix=$HOME/.localpython
+make && make install
+
+# setup proj dir
+cd ~/Documents
+mkdir proj && cd proj
+
+# setup virtualenv
+PY=$HOME/.localpython/bin/python3
+$PY -m venv venv
+source venv/bin/activate
+
+# pip install
+$PY -m pip install stellargraph chardet
